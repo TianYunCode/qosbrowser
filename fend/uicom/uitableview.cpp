e60c00385aeadb01dd98571db25fd1c8ab7a251d
wty <wty@mail.com>
2025-09-04 13:04:43 +0800

    暂时移除

--- fend/uicom/uitableview.cpp
+++
@@ -1,9 +0,0 @@
-#pragma execution_character_set("utf-8")
-
-#include "UiTableView.h"
-#include <fend/uidelegates/UiTableItemDelegate.h>
-
-UiTableView::UiTableView(QWidget *parent):QTableView(parent)
-{
-    setItemDelegate(new UiTableItemDelegate(this));
-}
--- middle/models/cloudmodels.cpp
+++
@@ -1,8 +0,0 @@
-#pragma execution_character_set("utf-8")
-
-#include "CloudModels.h"
-
-MyBucket::MyBucket()
-{
-
-}
